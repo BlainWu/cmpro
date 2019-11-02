44d5a1447ae8b7fb947083a231d09c7547d08068
kanae <oukanae@outlook.com>
2019-11-02 16:59:15 +0800

    coordinate convert

--- include/cmpro/coordinate_convert.h
+++ include/cmpro/coordinate_convert.h
@@ -6,10 +6,18 @@
 class CoordinateConvert {
 public:
     CoordinateConvert();
+    std::vector<int> multi_convert(std::vector<int> uvdata);
 private:
+    bool is_determined;
     //The convert of (u,v) -> (x,y)
-    std::vector<int> single_convert(int u,int v);
-    int tu,tv;
+    std::vector<double> single_convert(int u,int v);
+    //Coorinate Convert Paras input
+    void ccpin(int tu_,int tv_,int deltau_,int deltav_);
+
+    int x_int,y_int;
+    double x_double,y_double;
+
+    int tu,tv,deltau,deltav;
     double theta;
 };
 
--- src/coordinate_convert.cpp
+++ src/coordinate_convert.cpp
@@ -2,9 +2,42 @@
 
 
 CoordinateConvert::CoordinateConvert() {
-
+    is_determined = false;
 }
 
-std::vector<int> CoordinateConvert::single_convert(int u, int v) {
-    return std::vector<int>();
+std::vector<double> CoordinateConvert::single_convert(int u, int v) {
+    if(!is_determined){
+        return {0,0};
+    }
+    double beta = atan((v-tv)/(u-tu))-theta;
+    x_double = sqrt((u-tu)*(u-tu)+(v-tv)*(v-tv))*cos(beta);
+    y_double = sqrt((u-tu)*(u-tu)+(v-tv)*(v-tv))*sin(beta);
+    x_int = (int)x_double;
+    y_int = (int)y_double;
+    x_double = x_double*10/deltau;
+    y_double = y_double*10/deltau;
+    return {x_double,y_double};
 }
+
+void CoordinateConvert::ccpin(int tu_, int tv_, int deltau_, int deltav_) {
+    tu=tu_;
+    tv=tv_;
+    deltau=deltau_;
+    deltav=deltav_;
+    theta = atan(deltav/deltau);
+    is_determined = true;
+}
+
+std::vector<int> CoordinateConvert::multi_convert(std::vector<int> uvdata) {
+    std::vector<int> ret;
+    if(!is_determined){
+        return {0};
+    }
+    for(int i=0;i<68;i++){
+        single_convert(uvdata[2*i],uvdata[2*i+1]);
+        ret.push_back(x_double);
+        ret.push_back(y_double);
+    }
+    return ret;
+}
+
